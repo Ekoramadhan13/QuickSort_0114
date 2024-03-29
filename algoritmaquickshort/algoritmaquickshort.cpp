c8f8d1764bf066d1282536b1784b0f932650b1f9
Ekoramadhan13 <enugraha411@gmail.com>
2024-03-29 09:21:19 +0700

    void q_short

--- algoritmaquickshort/algoritmaquickshort.cpp
+++ algoritmaquickshort/algoritmaquickshort.cpp
@@ -36,5 +36,16 @@ void swap(int x, int y)
 	arr[x] = arr[y];
 	arr[y] = temp;
 	mov_count++;
+
+
 }
+
+void q_short(int low, int high)
+{
+	
+	int temp;
+	int pivot, i, j;
+	if (low > high) { //step 1
+		return;
+	}
 int main ()
